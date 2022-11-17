833fc5541050b794e34330ebe25d8ec0311e1fba
j r <rowla@LAPTOP-FC4R3S9N.localdomain>
2022-11-17 19:24:37 +0000

    moved to erc721a and added royalties

--- contracts/NFTCollection.sol
+++ contracts/NFTCollection.sol
@@ -1,85 +1,64 @@
-//SPDX-License-Identifier: MIT
-pragma solidity ^0.8.13;
 
-import "@openzeppelin/contracts/utils/Counters.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+    pragma solidity ^0.8.7;
 
-contract NFTCollection is ERC721Enumerable, Ownable {
-    using SafeMath for uint256;
-    using Counters for Counters.Counter;
+    import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+    import "@openzeppelin/contracts/access/Ownable.sol";
+    import "@openzeppelin/contracts/token/common/ERC2981.sol";
+    import "erc721a/contracts/ERC721A.sol";
 
-    Counters.Counter private _tokenIds;
-    // The max number of NFTs in the collection
-    uint public constant MAX_SUPPLY = 10;
-    // The mint price for the collection
-    uint public constant PRICE = 0.00001 ether;
-    // The max number of mints per wallet
-    uint public constant MAX_PER_MINT = 5;
+    contract SimpleContract is ERC721A, ERC2981, Ownable, ReentrancyGuard {
 
-    string public baseTokenURI;
+        uint256 public constant MAX_SUPPLY = 10000;
+        uint256 public constant MAX_MINTS_WALLET = 1;
 
-    constructor(string memory baseURI, string memory name, string memory symbol) ERC721(name, symbol) {
-        setBaseURI(baseURI);
-    }
+        string private _baseTokenURI;
 
+        bool public isSaleOpen;
 
-    function _baseURI() internal view virtual override returns (string memory) {
-        return baseTokenURI;
-    }
+        mapping (address => uint256) public totalMints;
 
-    function setBaseURI(string memory _baseTokenURI) public onlyOwner {
-        baseTokenURI = _baseTokenURI;
-    }
+        constructor(string memory tokenData) ERC721A("Simple Contract", "SICO") {
+            _baseTokenURI = tokenData;
+        }    
 
-    function mintNFTs(uint _count) public payable {
-        uint totalMinted = _tokenIds.current();
+        function toggleSaleOpen() external onlyOwner {
+            isSaleOpen = !isSaleOpen;
+        }
 
-        require(totalMinted.add(_count) <= MAX_SUPPLY, "This collection is sold out!");
-        require(_count >0 && _count <= MAX_PER_MINT, "You have received the maximum amount of NFTs allowed.");
-        require(msg.value >= PRICE.mul(_count), "Not enough ether to purchase NFTs.");
+        modifier callerIsUser() {
+            require(tx.origin == msg.sender, "Cannot be called by a contract");
+            _;
+        }
 
-        for (uint i = 0; i < _count; i++) {
-            _mintSingleNFT();
+        function freeMint() external callerIsUser nonReentrant {
+            require(isSaleOpen, "Sale not open");
+            require(MAX_SUPPLY > totalSupply(), "Sold out");
+            require((totalMints[msg.sender]) < MAX_MINTS_WALLET, "Only one free mint per wallet");
+            _safeMint(msg.sender, 1);
+            totalMints[msg.sender] += 1;
+        }
+
+        function _baseURI() internal view virtual override returns (string memory) {
+            return _baseTokenURI;
+        }
+
+        function withdraw() external onlyOwner nonReentrant {
+            (bool success, ) = msg.sender.call{value: address(this).balance}("");
+            require(success, "Transfer failed");
+        }
+
+        // ERC2981 functions
+
+        // @dev Required override
+        function supportsInterface(bytes4 interfaceId) public view virtual override(ERC721A, ERC2981) returns (bool) {
+            return super.supportsInterface(interfaceId);
+        }
+
+        function setDefaultRoyalty(address receiver, uint96 feeNumerator) external onlyOwner {
+            _setDefaultRoyalty(receiver, feeNumerator);
+        }
+
+        function deleteDefaultRoyalty() external onlyOwner {
+            _deleteDefaultRoyalty();
         }
     }
-
-    function _mintSingleNFT() private {
-        uint newTokenID = _tokenIds.current();
-        _safeMint(msg.sender, newTokenID);
-        _tokenIds.increment();
-    }
-    
-    // Returns the ids of the NFTs owned by the wallet address
-    function tokensOfOwner(address _owner) external view returns (uint[] memory) {
-        uint tokenCount = balanceOf(_owner);
-        uint[] memory tokensId = new uint256[](tokenCount);
-
-        for (uint i = 0; i < tokenCount; i++) {
-            tokensId[i] = tokenOfOwnerByIndex(_owner, i);
-        }
-        return tokensId;
-    }
-    
-    // Withdraw the ether in the contract
-    function withdraw() public payable onlyOwner {
-        uint balance = address(this).balance;
-        require(balance > 0, "No ether left to withdraw");
-
-        (bool success, ) = (msg.sender).call{value: balance}("");
-        require(success, "Transfer failed.");
-    }
-
-    // Reserve NFTs only for owner to mint for free
-    function reserveNFTs(uint _count) public onlyOwner {
-        uint totalMinted = _tokenIds.current();
-
-        require(totalMinted.add(_count) < MAX_SUPPLY, "Not enough NFTs left to reserve");
-
-        for (uint i = 0; i < _count; i++) {
-            _mintSingleNFT();
-        }
-    }
-}
-
